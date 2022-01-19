93765ff0fb6d59f2465b94b29bf1feaf39e06d29
dimpu124 <54626298+dimpu124@users.noreply.github.com>
2022-01-19 13:22:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/success/<name>')
-def success(name):
-    if name == "sona":
-        return 'No its Saumya'
-    else:
-        return 'Yes you are right its Saumya'
-
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   
-
-if __name__ == '__main__':
-   app.run()
+@app.route("/")
+def hello():
+    return "Hello, World!"
